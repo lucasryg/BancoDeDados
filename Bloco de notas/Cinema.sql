8e4a77a0bf08ac293f16f2f537d48d905b7693d1
lucasryg <lucasryg@gmail.com>
2020-02-17 16:01:23 -0300

    api

---
+++ Bloco de notas/Cinema.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE Filmes_tarde;
+
+USE Filmes_tarde;
+
+
+CREATE TABLE Generos(
+	IdGenero	INT PRIMARY KEY IDENTITY
+	,Nome		VARCHAR (255) NOT NULL UNIQUE
+);
+
+CREATE TABLE Filmes(
+	IdFilme		INT PRIMARY KEY IDENTITY
+	,Titulo		VARCHAR (255) NOT NULL UNIQUE
+	,IdGenero	INT FOREIGN KEY REFERENCES Generos (IdGenero)
+);	
+
+
+INSERT INTO Generos	(Nome)
+VALUES				('Ação')
+					,('Drama');
+					
+INSERT INTO Filmes	(Titulo, IdGenero)
+VALUES				('A vida é bela', 2)
+					,('Rambo', 1);
+
+					
+SELECT * FROM Generos;
+SELECT * FROM Filmes;
+
+SELECT IdGenero, Nome from Generos;
